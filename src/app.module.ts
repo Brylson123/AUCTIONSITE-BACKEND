55b693073c77c958b438d2aa66d834b94469b8f5
Bryla <csowscy@gmail.com>
2023-10-21 23:08:45 +0200

    add connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OffersModule } from './offers/offers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'utils/config/config.db';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.dbHost,
+      port: 3306,
+      username: config.dbUser,
+      password: config.dbPassword,
+      database: config.dbDatabase,
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
+    OffersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
