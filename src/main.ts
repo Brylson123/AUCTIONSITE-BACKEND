dcc13c7e51b85ae8bf3ccebc8e0c0bba1155e717
Bryla <csowscy@gmail.com>
2023-10-27 13:57:09 +0200

    Add cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,9 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.use(cookieParser());
+
   await app.listen(3001);
 }
 bootstrap();
