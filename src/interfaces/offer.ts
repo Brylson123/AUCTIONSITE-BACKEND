fb0adad8666d32385dd703a072d74554b2ca4d1e
Bryla <csowscy@gmail.com>
2023-10-27 14:02:06 +0200

    Change Interfaces

--- src/interfaces/offer.ts
+++ src/interfaces/offer.ts
@@ -1,6 +1,8 @@
-export interface OfferInterface {
+export type OfferInterface = {
   id: string;
   name: string;
   description: string;
   price: number;
-}
+};
+
+export type AddOfferResponse = OfferInterface;
