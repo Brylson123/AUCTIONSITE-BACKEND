2fda4adf5e2d198e2ec8ecac4d152c943e93d076
Bryla <csowscy@gmail.com>
2023-10-27 14:13:35 +0200

    Add

--- src/offers/dto/dto.ts
+++ src/offers/dto/dto.ts
@@ -1,8 +1,13 @@
-import { OfferInterface } from "../../../interfaces/offer";
+import { IsNumber, IsString, Max, Min } from 'class-validator';
 
-export class AddOfferDto implements OfferInterface{
+export class AddOfferDto {
+  @IsString()
+  @Max(999)
   description: string;
-  id: string;
+  @IsString()
+  @Max(99)
   name: string;
+  @IsNumber()
+  @Min(1)
   price: number;
 }
